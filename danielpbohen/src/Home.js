5ef4769fdd9036e485600a366a1375186bf4fb8e
Daniel Bohen <bohen@usc.edu>
2023-01-05 10:40:54 -0800

    make up to date

--- danielpbohen/src/Home.js
+++ danielpbohen/src/Home.js
@@ -32,9 +32,11 @@ export default function Home() {
         >
            <img src={me} alt="me" width="300em" height="auto" />
           <Typography sx={{mb: 2, ml: 2, mr: 2}}variant="h2">Daniel Bohen <Typography>
-            Hello! My name is Daniel Bohen, and I am a computer science student broadly interested in intelligent cyber physical systems. I am currently 
-            working for USC's Dr. Luis Garcia on NeuroIoT, focusing on the development of robust, explainable decision making systems for deep brain stimulation. 
-            I have a strong interest in the intersection of computer science and medicine, and hope to use explainable machine learning methods to facilitate 
+            Hello! My name is Daniel Bohen, and I am a computer science student broadly interested in intelligent cyber physical systems. Additionally, 
+            I have a strong interest in the intersection of computer science and medicine, and enjoy using machine learning and automation to assist with 
+            medical research projects and clinical tasks. I am currently working at the USC Information Sciences Institute for Dr. Luis Garcia on NeuroIoT. 
+            Our project is focusing on the development of robust, explainable decision making systems for deep brain stimulation. I plan to pursue a PhD in
+            Computer Science with a focus on modeling and control of complex dynamic systems. 
             
           </Typography></Typography>
         </Box>
